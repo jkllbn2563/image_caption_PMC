a573f6901aae9ce5fcc159bd0946ade771a37104
jkllbn2563 <jkllbn2563@yahoo.com.tw>
2019-08-08 15:29:02 +0800

    add the trigger

--- src/caption_pmc_server_test.py
+++ src/caption_pmc_server_test.py
@@ -13,7 +13,7 @@ def rgb_callback(image):
 	global img,receive_first_image
 	img=image
 def caption_calculation():	
-	global img
+	global img,caption
 	try:
 		
 		caption=caption_client(img)
@@ -21,6 +21,14 @@ def caption_calculation():
 		return caption
 	except rospy.ServiceException, e:
 		rospy.logwarn("service call failed:%s"%e)
+
+
+def handle_function_willie(req):
+	global caption
+
+
+	print("image caption is: ",caption.caption_result)
+	return caption.caption_result
 
 if __name__=='__main__':
 	rospy.init_node('client_caption_node',anonymous=True)
@@ -31,11 +39,20 @@ if __name__=='__main__':
 	
 	rospy.wait_for_service('image_caption')
 	caption_client=rospy.ServiceProxy('image_caption',imageCaptioning)
+	s=rospy.Service('triggerCaption',PMC_trigger,handle_function_willie)
+	detection_client_willie=rospy.ServiceProxy('triggerCaption',PMC_trigger)
+	rospy.wait_for_service('triggerCaption')
+
+
+
 	
 	rospy.loginfo('Ready to caption')
 
 	caption_result=caption_calculation()
 	print (type(caption_result))
 	print("The caption is :",caption_result.caption_result)
+
+	rospy.spin()
+
 
 	
