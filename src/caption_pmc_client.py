bdcab3778a36343b58776a42a9dea12ac1c93af8
Herobrixx <r07921014@ntu.edu.tw>
2019-08-08 20:26:05 +0800

    fixed service wrong logic

--- src/caption_pmc_client.py
+++ src/caption_pmc_client.py
@@ -3,54 +3,52 @@ import rospy
 from caption_pkg.srv import *
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
+from std_srvs.srv import Trigger, TriggerResponse
 import time
 
-caption_result=""
 bridge = CvBridge()
-
+caption = None
 img = None
+
 
 def rgb_callback(image):
 	global img,receive_first_image
 	img=image
+
+
 def caption_calculation():
 	global img,caption
 	try:
-
 		caption=caption_client(img)
-
-		return caption
 	except rospy.ServiceException, e:
 		rospy.logwarn("service call failed:%s"%e)
 
 
-def handle_function_willie(req):
+def executeCaption(req):
 	global caption
+	caption_calculation()
+	rospy.sleep(1.)
 
+	return TriggerResponse(
+        success=True,
+        message=caption.caption_result
+    )
 
-	print("image caption is: ",caption.caption_result)
-	return caption.caption_result
 
 if __name__=='__main__':
 	rospy.init_node('client_caption_node',anonymous=True)
-	#rospy.Subscriber("/c1/camera/rgb/image_raw",Image,rgb_callback)
+
+	# receice images
 	rospy.Subscriber("/scorpio/mmp0/base_camera/rgb/image_raw",Image,rgb_callback)
 	while img == None:
 		time.sleep(0.1)
 
+	# connect to image_caption service
 	rospy.wait_for_service('image_caption')
 	caption_client=rospy.ServiceProxy('image_caption',imageCaptioning)
-	s=rospy.Service('triggerCaption',PMC_trigger,handle_function_willie)
-	detection_client_willie=rospy.ServiceProxy('triggerCaption',PMC_trigger)
-	rospy.wait_for_service('triggerCaption')
 
+	# create triggerCaption server for main state machine trigger
+	triggerCaption_server=rospy.Service('triggerCaption',Trigger,executeCaption)
 
-
-
-	rospy.loginfo('Ready to caption')
-
-	caption_result=caption_calculation()
-	print (type(caption_result))
-	print("The caption is :",caption_result.caption_result)
-
+	rospy.loginfo('Ready to caption...')
 	rospy.spin()
